b4abbd83529e2f2bba35cf77a8233d7b42d11044
dinesh chaudhary <dinesh3029nitrr@gmail.com>
2024-01-27 15:55:36 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chest-Cancer-classification-Mlops-MLFlow-DVC-AWS"
+AUTHOR_USER_NAME = "dinesh"
+SRC_REPO = "cancerClassification"
+AUTHOR_EMAIL = "dinesh3029nitrr@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A  python package for cancer classfication app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
