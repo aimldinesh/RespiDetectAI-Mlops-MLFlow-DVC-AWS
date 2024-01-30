d5b0305348f709a4c2639502ecd53b5b9fcb0d45
dinesh chaudhary <dinesh3029nitrr@gmail.com>
2024-01-30 16:22:47 +0530

    added prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cancerClassification.config.configuration import ConfigurationManager
 from cancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cancerClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cancerClassification import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -14,3 +15,13 @@ except Exception as e:
     raise e
 
 
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
